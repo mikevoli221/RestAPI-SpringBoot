616f82af17a8102e8034ceba10bd18f34b6c0976
Hiep Ho <mikevoli221@yahoo.com>
2020-07-03 01:44:40 +0700

    update project

--- src/main/java/com/wirecard/person/PersonController.java
+++ src/main/java/com/wirecard/person/PersonController.java
@@ -1,4 +1,28 @@
 package com.wirecard.person;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
 public class PersonController {
+
+    @Autowired
+    private PersonServices services;
+
+    @GetMapping("/{id}")
+    public Person findPersonById(@PathVariable("id") String id){
+        return services.findPersonById(id);
+    }
+
+    @GetMapping
+    public List<Person> findAllPerson(){
+        return services.findAllPerson();
+    }
+
 }
--- src/main/java/com/wirecard/person/PersonServices.java
+++ src/main/java/com/wirecard/person/PersonServices.java
@@ -2,6 +2,36 @@ package com.wirecard.person;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Service
 public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    public Person findPersonById (String id){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Minh Hiep");
+        person.setLastName("Ho");
+        person.setAddress("HCMC, Vietnam");
+        person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAllPerson (){
+        List<Person> list = new ArrayList<Person>();
+        for (int i = 0; i <= 10; i++) {
+            Person person = new Person();
+            person.setId(counter.incrementAndGet());
+            person.setFirstName("Minh Hiep");
+            person.setLastName("Ho");
+            person.setAddress("HCMC, Vietnam");
+            person.setGender("Male");
+            list.add(person);
+        }
+        return list;
+    }
 }
