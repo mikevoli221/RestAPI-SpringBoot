a9f15939c1d958f55599c745e59bc3dd459b00d2
Wirecard Vietnam <hiep.ho@wirecard.com>
2020-07-07 16:45:40 +0700

    update version

--- src/main/java/com/wirecard/person/PersonController.java
+++ src/main/java/com/wirecard/person/PersonController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/v1/person")
 public class PersonController {
 
     private static Logger logger = LoggerFactory.getLogger(PersonController.class);
---
+++ src/main/java/com/wirecard/person/PersonControllerV2.java
@@ -0,0 +1,23 @@
+package com.wirecard.person;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v2/person")
+public class PersonControllerV2 {
+    private static Logger logger = LoggerFactory.getLogger(PersonControllerV2.class);
+
+    @Autowired
+    private PersonServices services;
+
+    @PostMapping
+    public PersonDTO createPerson (@RequestBody PersonDTO personDTO){
+        return services.createPerson(personDTO);
+    }
+}
